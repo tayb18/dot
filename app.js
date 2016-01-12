3e968e03d69909cc563d8ea1f55e7940ed91c136
isabel cusick <cusick.isabel@gmail.com>
2016-01-12 14:14:13 -0500

    passport

--- app.js
+++ app.js
@@ -2,7 +2,22 @@ var express = require('express');
 var app = express();
 var methodOverride = require('method-override');
 var passport = require('passport'),
-LocalStrategy = require('passport-local').Strategy,
+LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
