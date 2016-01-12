302c81187d7cd50d852aa0aa8c4437d2efeef048
isabel cusick <cusick.isabel@gmail.com>
2016-01-12 14:04:13 -0500

    passport

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var methodOverride = require('method-override');
-// var passport = require('passport'),
-// LocalStrategy = require('passport-local'),
+var passport = require('passport'),
+LocalStrategy = require('passport-local').Strategy,
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
@@ -39,6 +39,13 @@ app.get('/calendar', function(req, res){
   res.render('calendar');
 });
 
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
 
 
 app.listen(process.env.PORT || 3000);
